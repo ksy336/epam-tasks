a3f57af9851fd83f210879b11a3f7cc7891e75f3
ksy336 <ksuoxy773@gmail.com>
2021-12-20 11:22:28 +0400

    feat: add resolved rectangle, triangle, another triangle

---
+++ Task_1/Task_1.1/Task_1.1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string str = "";
+            do
+            {
+                Console.WriteLine("Введите любое число: ");
+                str = Console.ReadLine();
+            } while (!int.TryParse(str, out n));
+            Console.Clear();
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                    if (j == i - 1)
+                        Console.WriteLine("");
+                }
+            }
+
+        }
+    }
+}
