44876539ceb9243a0dbeacd14f0d8dfc32405c02
ksy336 <ksuoxy773@gmail.com>
2021-12-20 15:17:43 +0400

    feat: resolve x-tree task

---
+++ Task_1/Task_1.1/Task_1.1.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string str;
+            do
+            {
+                Console.WriteLine("Введите число: ");
+                str = Console.ReadLine();
+            } while (!int.TryParse(str, out n));
+            Console.Clear();
+
+
+            for(int i = 1; i <= n; i++)
+            {
+                for(
+                    var j = 0; j <= i; j++)
+                {
+                    var branch = new String('*', j);
+                    Console.WriteLine(branch.PadLeft(n) + "*" + branch);
+                }
+            }
+               
+        }
+    }
+}
+
